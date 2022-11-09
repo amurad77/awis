bed563af32979774ad7a0b85de35697735d87914
sheyda99 <sheydasultan.99@gmail.com>
2022-11-09 20:34:39 +0400

    updating subscribe design & functionality

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -1,6 +1,4 @@
-
-let form = document.getElementById('subscribe-form' );
-
+let form = document.getElementById('subscribe-form');
 let error_section =  document.getElementById('error-list');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
@@ -15,29 +13,24 @@ form.addEventListener('submit', async function (e) {
         },
         method: 'POST',
         body: JSON.stringify(form_data)
-    })
+    });
     let response_data = await response.json();
     let status = await response.ok;
-    
+
     if(status === true){
-        alert('Ugurla abone oldunuz');
-        document.getElementById('mce-EMAIL').value = ''
-    }
-    else{
+        error_section.innerHTML += "<li class='text-success'>You have successfully subscribed.</li>";
+        document.getElementById('mce-EMAIL').value = '';
+    } else {
         for(let error of response_data.email) {
-           error_section.innerHTML += `<li class="text-danger">${error}</li>`  
+           error_section.innerHTML += `<li class="text-danger">${error}</li>`;
         }
     }
+    setTimeout(function () {
+        error_section.innerHTML = "";
+    }, 5000);
+});
 
-}) 
-
-
-
-
-
-
-let form2 = document.getElementById('form-subscribe' );
-
+let form2 = document.getElementById('form-subscribe');
 let error_section2 =  document.getElementById('error-list2');
 form2.addEventListener('submit', async function (e) {
     e.preventDefault();
@@ -52,18 +45,19 @@ form2.addEventListener('submit', async function (e) {
         },
         method: 'POST',
         body: JSON.stringify(form_data)
-    })
+    });
     let response_data = await response.json();
     let status = await response.ok;
-    
-    if(status === true){
-        alert('Ugurla abone oldunuz');
-        document.getElementById('mce-EMAIL2').value = ''
-    }
-    else{
+
+    if (status === true) {
+        error_section2.innerHTML += "<li class='text-success'>You have successfully subscribed.</li>";
+        document.getElementById('mce-EMAIL2').value = '';
+    } else {
         for(let error of response_data.email) {
-           error_section2.innerHTML += `<li class="text-danger">${error}</li>`  
+            error_section2.innerHTML += `<li class="text-danger">${error}</li>`;
         }
     }
-
-}) 
+    setTimeout(function () {
+        error_section2.innerHTML = "";
+    }, 5000);
+});
