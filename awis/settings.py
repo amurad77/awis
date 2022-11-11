d3a129d37ace022cc5aef328f05de974f930d7b3
amurad77 <amurad7799@gmail.com>
2022-11-11 23:49:26 +0400

    settings change

--- awis/settings.py
+++ awis/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if PROD:
-    STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+    STATIC_ROOT = os.path.join(BASE_DIR, "./static/")
 else:
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "./static/")
