d8deeea49520b8e72656c68647109ba682309e1a
amurad77 <amurad7799@gmail.com>
2022-11-11 22:30:44 +0400

    debug change

--- awis/settings.py
+++ awis/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7szm0lcpfxgd!!@3b&f$pj9g&oz#d6@49jyt!e#henj=4id0rw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
