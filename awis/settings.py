f58ce0754d947a58837f87a9a453400def287b90
amurad77 <amurad7799@gmail.com>
2022-11-14 00:07:44 +0400

    end fix

--- awis/settings.py
+++ awis/settings.py
@@ -23,8 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7szm0lcpfxgd!!@3b&f$pj9g&oz#d6@49jyt!e#henj=4id0rw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ.get('DEBUG') else True
-PROD = not DEBUG
+DEBUG = True
+# DEBUG = False if os.environ.get('DEBUG') else True
+# PROD = not DEBUG
 
 ALLOWED_HOSTS = ['*']
 
@@ -150,19 +151,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
-
-if PROD:
-    STATIC_ROOT = os.path.join(BASE_DIR, "./staticfiles/")
-else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "./staticfiles/")
-    ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+# if PROD:
+#     STATIC_ROOT = os.path.join(BASE_DIR, "./staticfiles/")
+# else:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, "./staticfiles/")
+#     ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media')
+)
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
