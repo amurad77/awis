dfb87839699e1c785b9d9bd9499ba4777b18f0bd
sheyda99 <sheydasultan.99@gmail.com>
2022-11-14 21:36:07 +0400

    updating subscribe form

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -29,35 +29,3 @@ form.addEventListener('submit', async function (e) {
         error_section.innerHTML = "";
     }, 5000);
 });
-
-let form2 = document.getElementById('form-subscribe');
-let error_section2 =  document.getElementById('error-list2');
-form2.addEventListener('submit', async function (e) {
-    e.preventDefault();
-    error_section2.innerText = '';
-    let form_data = {
-        'email': form2.email.value
-    }
-    let response = await fetch('http://188.132.130.34/api/subscribe/', {
-        headers: {
-            'content-type': 'application/json',
-            "X-CSRFToken": form.csrfmiddlewaretoken.value
-        },
-        method: 'POST',
-        body: JSON.stringify(form_data)
-    });
-    let response_data = await response.json();
-    let status = await response.ok;
-
-    if (status === true) {
-        error_section2.innerHTML += "<li class='text-success'>You have successfully subscribed.</li>";
-        document.getElementById('mce-EMAIL2').value = '';
-    } else {
-        for(let error of response_data.email) {
-            error_section2.innerHTML += `<li class="text-danger">${error}</li>`;
-        }
-    }
-    setTimeout(function () {
-        error_section2.innerHTML = "";
-    }, 5000);
-});
